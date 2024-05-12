0ecbbcec10212bdcb6101418a5419970493a31af
Dillon Beliveau <dillonbeliveau@gmail.com>
2024-05-11 19:04:58 -0700

    Attempt to only split at the section level

--- update.py
+++ update.py
@@ -17,24 +17,43 @@ if token is None or token == "":
     print("No token found")
     exit(1)
 
+def chunk_str(s, split_on, backup_split_on):
+    messages = []
+
+    chunks = s.split(split_on)
+    while len(chunks) > 0:
+        if len(chunks[0]) > MAX_MESSAGE_LENGTH:
+            if backup_split_on is None:
+                print(f"Message too long to split: {len(chunks[0])}")
+                print(f"Bad message:\n\n{chunks[0]}")
+                exit(1)
+
+            messages += chunk_str(chunks.pop(0), backup_split_on, None)
+            continue
+
+        message = chunks.pop(0)
+
+        while len(chunks) > 0 and len(message + split_on + chunks[0]) < MAX_MESSAGE_LENGTH:
+            message += split_on + chunks.pop(0)
+
+        messages.append(message)
+        print(f"Message of size {len(message)} added")
+
+    return messages
+
+def get_split_contents(filename, split_on="\n\n", backup_split_on="\n"):
+    print(f"Processing file {filename}")
+    with open(filename, "r") as f:
+        return chunk_str(f.read(), split_on, backup_split_on)
+
+# Load and process each file
+for channel_config in config:
+    channel_config["messages"] = get_split_contents(channel_config["file"])
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
-
-def get_split_contents(filename):
-    split_on = "\n"
-    with open(filename, "r") as f:
-        messages = []
-        chunks = f.read().split(split_on)
-        while len(chunks) > 0:
-            message = ""
-            while len(chunks) > 0 and len(message + split_on + chunks[0]) < MAX_MESSAGE_LENGTH:
-                message += split_on + chunks.pop(0)
-            messages.append(message)
-        return messages
-
 
 @client.event
 async def on_ready():
@@ -57,8 +76,8 @@ async def on_ready():
                 await message.delete()
 
         # Send new messages from the file
-        for m in get_split_contents(channel_config["file"]):
-            await channel.send(m,
+        for message in channel_config["messages"]:
+            await channel.send(message,
                                # No link previews
                                suppress_embeds=True,
                                # Don't send any notifications
